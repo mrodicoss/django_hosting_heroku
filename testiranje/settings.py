61f28713bffd5490559cc4a37faf84d487efef45
mrodicoss <mrodic@oss.unist.hr>
2018-05-20 18:45:26 +0200

    debug mode false ALLOWED_HOSTS = ['*']

--- testiranje/settings.py
+++ testiranje/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+az23ww@i!z4xsjts7n0-2s_xmwfq%c6ig^fd2u&!%&&fm=05!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
